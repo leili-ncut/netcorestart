68c4b18abfc4b6b62cdf6fb5cb5f931ed3bad83e
lilei-ncut/netcoredemo.git <baqiaoll007@163.com>
2017-03-08 23:01:09 +0800

    start

---
+++ src/StartupDemo/RequestLoggerMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace StartupDemo
+{
+    public class RequestLoggerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public RequestLoggerMiddleware(RequestDelegate next,ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<RequestLoggerMiddleware>();
+
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            _logger.LogInformation("Handling request:" + context.Request.Path);
+            await _next.Invoke(context);
+            _logger.LogInformation("Finished handling request");
+        }
+
+    }
+
+    public static class RequestLoggerExtensions
+    {
+        public static IApplicationBuilder UseRequestLogger(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestLoggerMiddleware>();//手工高亮
+        }
+    }
+}
---
+++ src/StartupDemo/Startup.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using StartupDemo.Data;
+using StartupDemo.Models;
+using StartupDemo.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+
+namespace StartupDemo
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets();
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddMvc();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, AuthMessageSender>();
+            services.AddTransient<ISmsSender, AuthMessageSender>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            var logger = loggerFactory.CreateLogger("test");
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()                                      //手工高亮
+            {                                                                               //手工高亮
+                FileProvider = new PhysicalFileProvider(                                    //手工高亮
+            Path.Combine(Directory.GetCurrentDirectory(), @"MyStaticFiles")),       //手工高亮
+                RequestPath = new PathString("/StaticFiles")                                //手工高亮
+            });
+
+            app.UseIdentity();
+
+            // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
+            app.UseRequestLogger();
+            ConfigureMapWhen(app);
+            app.Use(async (context, next) =>//手工高亮
+            {
+                logger.LogInformation("Handling request.");
+                await next.Invoke();//手工高亮
+                logger.LogInformation("Finished handling request.");
+            });
+            app.Use(async (x, next) =>
+            {
+                await next.Invoke();
+                await x.Response.WriteAsync("Hello from 222");
+            });
+            app.Map("/maptest", HandleMapTest);
+
+            //app.UseCors();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        private static void HandleMapTest(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Map Test Successful");
+            });
+
+        }
+        public void ConfigureMapWhen(IApplicationBuilder app)
+        {
+            app.MapWhen(context =>
+            {//手工高亮
+                return context.Request.Query.ContainsKey("branch");//手工高亮
+            }, HandleBranch);//手工高亮
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello from 111");
+            });
+        }
+        private static void HandleBranch(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Branch used.");//手工高亮
+            });
+        }
+    }
+}
